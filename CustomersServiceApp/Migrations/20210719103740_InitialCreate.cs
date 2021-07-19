ca68626968d4ee79f8f4c14a2c6e26d4f2c17dcd
Bartek Tusiński <b.tusinski27@gmail.com>
2021-07-19 14:24:25 +0200

    Added migrations.

---
+++ CustomersServiceApp/Migrations/20210719103740_InitialCreate.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace CustomersServiceApp.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "customer",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "text", nullable: true),
+                    surname = table.Column<string>(type: "text", nullable: true),
+                    birthyear = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_customer", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "customer");
+        }
+    }
+}
--- CustomersServiceApp/Models/Customer.cs
+++ CustomersServiceApp/Models/Customer.cs
@@ -3,14 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 
 namespace CustomersServiceApp.Models
 {
     public class Customer
     {
+        [Required]
         public int id { get; set; }
+
+        [StringLength(50, ErrorMessage = "Name cannot be longer than 50 characters.")]
+        [Display(Name = "Name")]
+        [Required]
         public string name { get; set; }
+
+        [StringLength(50, ErrorMessage = "Surname cannot be longer than 50 characters.")]
+        [Display(Name = "Surname")]
+        [Required]
         public string surname { get; set; }
+
+        [Display(Name = "Birth Year")]
+        [Required]
         public int birthyear { get; set; }
+
+        [Display(Name = "Full Name")]
+        public string FullName
+        {
+            get
+            {
+                return name + ", " + surname;
+            }
+        }
     }
 }
